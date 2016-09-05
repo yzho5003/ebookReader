15dc667bec928dc30a3fcbfcaf9be67c85c14fdb
yzho5003 <yzho5003@uni.sydney.edu.au>
2016-09-05 22:06:22 +1000

    Sync Test

--- EbookReader/app/src/main/java/comp5216/sydney/edu/au/ebookreader/textSelectTest.java
+++ EbookReader/app/src/main/java/comp5216/sydney/edu/au/ebookreader/textSelectTest.java
@@ -2,13 +2,31 @@ package comp5216.sydney.edu.au.ebookreader;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+<<<<<<< Updated upstream
 import android.view.ActionMode;
 import android.view.ActionMode.Callback;
 import android.view.Menu;
 import android.view.MenuItem;
+=======
+import android.text.Spannable;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.util.Log;
+import android.view.View;
+>>>>>>> Stashed changes
 import android.widget.TextView;
 import android.widget.Toast;
 
+<<<<<<< Updated upstream
+=======
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Locale;
+
+>>>>>>> Stashed changes
 public class textSelectTest extends AppCompatActivity {
 
     private static final int TRANSLATE = 1;
@@ -129,4 +147,31 @@ public class textSelectTest extends AppCompatActivity {
 
         });
     }
+
+    protected void copyDataBase() throws IOException {
+
+        //Open your local db as the input stream
+        InputStream myInput = getApplicationContext().getAssets().open("file.db");
+
+        // Path to the just created empty db
+        String outFileName = "/data/data/com.yourpackagename/databases/" + "file.db";
+
+        //Open the empty db as the output stream
+        OutputStream myOutput = new FileOutputStream(outFileName);
+
+        //transfer bytes from the inputfile to the outputfile
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = myInput.read(buffer))>0){
+            myOutput.write(buffer, 0, length);
+        }
+
+        //Close the streams
+        myOutput.flush();
+        myOutput.close();
+        myInput.close();
+
+    }
+
+
 }
