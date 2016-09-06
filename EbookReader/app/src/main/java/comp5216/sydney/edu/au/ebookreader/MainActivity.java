34daca49e3e9cd83c08c5d5fe1d9520cd33d7b50
yzho5003 <yzho5003@uni.sydney.edu.au>
2016-09-06 10:42:26 +1000

    Set the launch screen with buttons to each test activity
    Please add a single activity for each testing part

--- EbookReader/app/src/main/java/comp5216/sydney/edu/au/ebookreader/MainActivity.java
+++ EbookReader/app/src/main/java/comp5216/sydney/edu/au/ebookreader/MainActivity.java
@@ -1,7 +1,9 @@
 package comp5216.sydney.edu.au.ebookreader;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void textSelectionOnClick(View view){
+        Intent textSelectTest = new Intent(MainActivity.this, TextSelectTest.class);
+
+        if(textSelectTest != null){
+            startActivity(textSelectTest);
+        }
     }
 }
